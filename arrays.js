bec4c2203a40198f489f647cf5c18e86fa8fc75f
Allen <developer2019@icloud.com>
2020-05-08 02:53:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, element) {
   let newArr = arr.slice();
-  newArr.push(element);
+  newArr.unshift(element);
   return newArr;
 }
 
