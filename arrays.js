28875aafe13b4d8b38fb48be1b904e9937806aa1
Allen <developer2019@icloud.com>
2020-05-08 02:27:13 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+const addElementToBeginningOfArray = (arr, element) => {
+  arr.unshift(element);
+  return arr;
+}
+
+const destructivelyAddElementToBeginningOfArray = (arr, element) => {
+  let newArr = arr;
+  newArr.unshift(element);
+  return newArr;
+}
+
+const addElementToEndOfArray = (arr, element) => {
+  let newArr = arr;
+  newArr.push(element);
+  return newArr;
+}
+
+const destructivelyAddElementToEndOfArray = (arr, element) => {
+  arr.push(element);
+  return arr;
+}
+
+const accessElementInArray = (arr, idx) => {
+  return arr[idx];
+}
+
+const destructivelyRemoveElementFromBeginningOfArray = (arr) => {
+  arr.shift();
+  return arr;
+}
+
+const removeElementFromBeginningOfArray = (arr) => {
+  return arr.slice(1);
+}
+
+const destructivelyRemoveElementFromEndOfArray = (arr) => {
+   arr.pop();
+   return arr
+}
+
+const removeElementFromEndOfArray = (arr) => {
+  return arr.slice(0, arr.length - 1);
+}
